a26ab928ad6bc437b881d3901d74c9e1ed3e89d4
Thu CAO <cao.trung.thu@gmail.com>
2019-06-16 16:21:23 +0700

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,6 @@ module.exports = {
   apps : [{
     name      : 'diego-service',
     script    : 'dist/app.js',
-    exec_mode: 'cluster',
-    instances: "max",
     env: {
       name : 'diego-rsmq',
       NODE_ENV: 'development',
--- src/app.js
+++ src/app.js
@@ -1,55 +1,40 @@
 import cors from 'cors'
 import { urlencoded, json } from 'body-parser'
 import dotenv from 'dotenv'
-import redisClient from './redis-client'
 
 dotenv.load()
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-app.use(urlencoded({ extended: true, limit: '500mb'}))
-app.use(json({ extended: true, limit: '500mb'}))
+app.use(urlencoded({ extended: true, limit: '500mb' }))
+app.use(json({ extended: true, limit: '500mb' }))
 app.use(cors())
 
 http.listen(process.env.PORT)
 
 app.get('/', (_, res) => {
 	res.send('Diego Cao: Hello')
-  })
+})
 
-  io.on('connection', (socket) => {
-	console.log('a user connected')
+io.on('connection', (socket) => {
+	var uid = ""
+	console.log('user connected')
 	socket.on('disconnect', () => {
 		console.log('user disconnected')
-	  })
-  })
+	})
 
-redisClient.rsmq.createQueue({qname:"myqueue"}, function (_err, resp) {
-  if (resp===1) {
-    console.log("queue created")
-  }
-})
+	socket.on("user_join", (data) => {
+		uid = data
+		console.log('uid: ' + uid)
+		socket.broadcast.emit("user_join", data)
+	})
 
-redisClient.rsmq.sendMessage({qname:"myqueue", message:"Hello World"}, function (_err, resp) {
-	if (resp) {
-		console.log("Message sent. ID:", resp);
-	}
-})
+	socket.on("chat_message", (data) => {
+		data.uid = uid
+		console.log('data: ')
+		console.log(data)
+		socket.emit("chat_message", data)
+	})
 
-redisClient.rsmq.listQueues( function (err, queues) {
-	if( err ){
-		console.error( err )
-		return
-	}
-	console.log("Active queues: " + queues.join( "," ) )
-})
-
-redisClient.rsmq.receiveMessage({qname:"myqueue"}, function (_err, resp) {
-	if (resp !== null && resp !== undefined && resp.id) {
-		console.log("Message received.", resp)	
-	}
-	else {
-		console.log("No messages for me...")
-	}
 })
